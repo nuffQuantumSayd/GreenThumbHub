4f989cbf48ba4b399a1c3386eb20ccbbcd24221c
aaron <aaronkornish.ak@gmail.com>
2024-07-16 16:40:27 -0700

    Imported PlantsController and used apiGetPlants in router get request

--- backend/api/plants.route.js
+++ backend/api/plants.route.js
@@ -1,7 +1,9 @@
 import express from 'express'
+import PlantsController from './plants.controller.js'
 
 const router = express.Router()
 
-router.route('/').get((req,res) => res.send('hello world'))
+// router.route('/').get((req,res) => res.send('hello world'))
+router.route('/').get(PlantsController.apiGetPlants)
 
 export default router
