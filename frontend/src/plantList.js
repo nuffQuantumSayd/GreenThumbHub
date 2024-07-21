78caf68db7a0274daf7afaace8ef250c6ac3ffb6
aaron <aaronkornish.ak@gmail.com>
2024-07-21 11:15:43 -0700

    Created a list of plants to use in the project.

---
+++ frontend/src/plantList.js
@@ -0,0 +1,74 @@
+const plants = [
+    {
+        _id: "1",
+        image: "/images/calathea-plant.png",
+        title: "Calathea Plant",
+        description: "A beautiful plant with unique foliage patterns.",
+        price: "29.99"
+    },
+    {
+        _id: "2",
+        image: "/images/spear-leaf-plant.png",
+        title: "Snake Plant",
+        description: "An easy-care plant that thrives in low light conditions.",
+        price: "19.99"
+    },
+    {
+        _id: "3",
+        image: "/images/philodendron-plant.png",
+        title: "Monstera Deliciosa",
+        description: "Known for its large, split leaves, perfect for any indoor space.",
+        price: "39.99"
+    },
+    {
+        _id: "4",
+        image: "/images/aloe-vera.png",
+        title: "Spider Plant",
+        description: "A popular choice for its air-purifying properties.",
+        price: "15.99"
+    },
+    {
+        _id: "5",
+        image: "/images/cactus-plant.png",
+        title: "Peace Lily",
+        description: "An elegant plant with white blooms and dark green leaves.",
+        price: "24.99"
+    },
+    {
+        _id: "6",
+        image: "/images/Rose-Gold-Feminine-Calligraphy.png",
+        title: "Philodendron",
+        description: "A versatile plant with heart-shaped leaves. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
+        price: "27.99"
+    },
+    {
+        _id: "7",
+        image: "/images/zz_plant.jpg",
+        title: "ZZ Plant",
+        description: "Low maintenance and drought-tolerant, perfect for busy individuals.",
+        price: "34.99"
+    },
+    {
+        _id: "8",
+        image: "/images/aloe-vera.png",
+        title: "Aloe Vera",
+        description: "Not only attractive, but also known for its healing properties. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
+        price: "14.99"
+    },
+    {
+        _id: "9",
+        image: "/images/pothos.jpg",
+        title: "Pothos",
+        description: "A hardy vine that's easy to grow in various conditions.",
+        price: "12.99"
+    },
+    {
+        _id: "10",
+        image: "/images/fern.jpg",
+        title: "Boston Fern",
+        description: "A lush plant that adds a touch of greenery to any room.",
+        price: "22.99"
+    }
+];
+
+export default plants
