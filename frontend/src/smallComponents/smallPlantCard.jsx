10723cfad4119525d84be2fe1c9ffcebf8ea2427
aaron <aaronkornish.ak@gmail.com>
2024-07-21 11:45:12 -0700

    Added truncate text to the description in SmallPlantCard

--- frontend/src/smallComponents/smallPlantCard.jsx
+++ frontend/src/smallComponents/smallPlantCard.jsx
@@ -7,7 +7,7 @@ const SmallPlantCard = ({ title, img, description, price }) => {
 			<img src={img} />
 			<div className="small-plant-card-info-group">
 				<p className="small-plant-card-title">{title}</p>
-				<p className="small-plant-card-description">{description}</p>
+				<p className="small-plant-card-description truncate">{description}</p>
 				<div className="small-plant-card-button-group">
 					<p className="small-plant-card-price">${price}</p>
 					<CommonButton title={buttonTitle} />
