87b4c665e1bfd6265b9577ecc3d360b8c60c6dab
aaron <aaronkornish.ak@gmail.com>
2024-07-23 21:17:25 -0700

    Added getPlantById function to PlantsDAO

--- backend/dao/PlantsDAO.js
+++ backend/dao/PlantsDAO.js
@@ -1,4 +1,5 @@
 let plants
+import mongodb from "mongodb"
 
 export default class PlantsDAO {
     static async injectDB(conn) {
@@ -21,10 +22,19 @@ export default class PlantsDAO {
         try {
             cursor  = await plants.find()
             let plantList = await cursor.toArray()
-            
             return plantList
         } catch (e) {
             console.log(`unable to issue find command, ${e}`)
+        }
+    }
+
+    static async getPlantById(id) {
+        try {
+            const plantById = await plants.findOne({_id: new mongodb.ObjectId(id)})
+            return plantById
+        } catch (e) {
+            console.log(`unable to issue find command ${e}`)
+            throw e
         }
     }
 }
