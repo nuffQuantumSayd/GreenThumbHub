0d4be71491b9c6edfab84385484d1383ac287c83
aaron <aaronkornish.ak@gmail.com>
2024-07-23 21:18:17 -0700

    Created api functions for get all plants and get plant by id

--- backend/api/plants.controller.js
+++ backend/api/plants.controller.js
@@ -1,10 +1,28 @@
 import PlantsDAO from "../dao/PlantsDAO.js";
 
-export default class PlantsController {
-    static async apiGetPlants(req, res, next) {
-        
-        let response = await PlantsDAO.getPlants()
 
-        res.json(response)
-    }
+export default class PlantsController {
+	static async apiGetPlants(req, res, next) {
+		try {
+			let response = await PlantsDAO.getPlants();
+
+			res.json(response);
+		} catch (e) {
+			console.log(e);
+			res.status(200).send(e.message);
+		}
+	}
+
+	static async apiGetPlantById(req, res, nex) {
+		try {
+
+            let {id} = req.params
+            let plantById = await PlantsDAO.getPlantById(id)
+  
+            res.json(plantById)
+		} catch (e) {
+            console.log(e)
+            res.status(404).send(e.message)
+        }
+	}
 }
