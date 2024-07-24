c59e6e35457bbf41aca2406a2889ca8bf0b52e5f
aaron <aaronkornish.ak@gmail.com>
2024-07-23 20:09:37 -0700

    Added reviews file to project

---
+++ frontend/src/reviews.js
@@ -0,0 +1,32 @@
+const reviews = [
+    {
+        _id: 1,
+        image: '/images/rating-profile-1.png',
+        name: 'Alina Patel',
+        rating: 4.5,
+        review: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,',
+    },
+    {
+        _id: 2,
+        image: '/images/rating-profile-1.png',
+        name: 'Maln Josi',
+        rating: 4.5,
+        review: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,',
+    },
+    {
+        _id: 3,
+        image: '/images/rating-profile-1.png',
+        name: 'Alina Thakur',
+        rating: 4.5,
+        review: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,',
+    },
+    {
+        _id: 4,
+        image: '/images/rating-profile-1.png',
+        name: 'Max Makvana',
+        rating: 4.5,
+        review: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,',
+    }
+]
+
+export default reviews
