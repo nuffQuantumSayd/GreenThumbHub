72ce4618eba3cbf90fbcbb237b384a907c41aeb5
aaron <aaronkornish.ak@gmail.com>
2024-07-24 18:44:11 -0700

    Created api function to create plant

--- backend/api/plants.controller.js
+++ backend/api/plants.controller.js
@@ -9,20 +9,36 @@ export default class PlantsController {
 			res.json(response);
 		} catch (e) {
 			console.log(e);
-			res.status(200).send(e.message);
+			res.status(500).send(e.message);
 		}
 	}
 
-	static async apiGetPlantById(req, res, nex) {
+	static async apiGetPlantById(req, res, next) {
 		try {
 
             let {id} = req.params
             let plantById = await PlantsDAO.getPlantById(id)
-  
             res.json(plantById)
 		} catch (e) {
             console.log(e)
-            res.status(404).send(e.message)
+            res.status(500).send(e.message)
         }
 	}
+
+	static async apiCreatePlant(req, res, next) {
+		try {
+			const image = req.body.image
+			const name = req.body.name
+			const desc = req.body.desc
+			const price = req.body.price
+
+			await PlantsDAO.createPlant(image,name,desc,price)
+
+			res.json({status: "success"})
+		} catch (e) {
+			console.log(e)
+			res.status(500).send(e.message)
+		}
+	}
+
 }
