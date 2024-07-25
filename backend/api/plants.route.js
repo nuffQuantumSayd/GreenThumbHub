25385dcbdbe7bb38eb898765986495794ce33be8
aaron <aaronkornish.ak@gmail.com>
2024-07-24 19:01:02 -0700

    Added delete plant to router

--- backend/api/plants.route.js
+++ backend/api/plants.route.js
@@ -4,8 +4,16 @@ import PlantsController from './plants.controller.js'
 const router = express.Router()
 
 // router.route('/').get((req,res) => res.send('hello world'))
+//get all plants
 router.route('/').get(PlantsController.apiGetPlants)
+
+//get plant by id
 router.route('/:id').get(PlantsController.apiGetPlantById)
+
+//create plant
 router.route('/').post(PlantsController.apiCreatePlant)
+
+//delete plant by id
+router.route('/:id').delete(PlantsController.apiDeletePlant)
 
 export default router
