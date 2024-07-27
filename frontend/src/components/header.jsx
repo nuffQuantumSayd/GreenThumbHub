c9d12bd88141f80675af225ca3e1639cc02a9539
aaron <aaronkornish.ak@gmail.com>
2024-07-26 20:16:30 -0700

    Created a responsive mobile menu

--- frontend/src/components/header.jsx
+++ frontend/src/components/header.jsx
@@ -6,6 +6,14 @@ import menuButton from "/images/icons/menu-button.png"
 const Header = () => {
 	const brand = "Planto.";
     
+    const headerHandler = () => {
+        let x = document.querySelector('.nav-menu-one')
+        if(x.style.display === 'block') {
+          x.style.display = 'none'
+        } else {
+          x.style.display = 'block'
+        }
+      }
 
 
 	return (
@@ -14,16 +22,16 @@ const Header = () => {
 				<img src={plantIcon} />
 				<p>{brand}</p>
 			</div>
+            <div className="nav-menu-two">
+                <a href="#"><img src={search}/></a>
+                <a className="nav-menu-two-shift-left" href="#"><img src={bag}/></a>
+                <a id="menu-button" className="nav-menu-two-shift-left" href="#" onClick={headerHandler}><img src={menuButton}/></a>
+            </div>
             <div className="nav-menu-one">
                 <p>Home</p>
                 <p>Plant Type</p>
                 <p>More</p>
                 <p>Contact</p>
-            </div>
-            <div className="nav-menu-two">
-                <a href="#"><img src={search}/></a>
-                <a className="nav-menu-two-shift-left" href="#"><img src={bag}/></a>
-                <a className="nav-menu-two-shift-left" href="#"><img src={menuButton}/></a>
             </div>
 		</header>
 	);
